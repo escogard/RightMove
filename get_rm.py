10e9b3ce49e305511fce1ea5720f32ec862f2993
escogard <escogar.d@gmail.com>
2021-04-08 16:13:30 +0100

    Add folder creation
    Add folder creation in get_rm.py and fix rent function call

--- get_rm.py
+++ get_rm.py
@@ -1,12 +1,24 @@
 from RM_scrapper import rms_buy, rms_rent
 from locations_dict import boroughs
 from datetime import date
+import os
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+
+if not os.path.exists('data/buy'):
+    os.makedirs('data/buy')
+
+if not os.path.exists('data/rent'):
+    os.makedirs('data/rent')
+
 
 today = date.today().strftime('%Y%m%d')
 
 for borough, loc in boroughs.items():
     listing_buy = rms_buy(borough,loc)
-    listing_rent = rms_buy(borough,loc)
+    listing_rent = rms_rent(borough,loc)
     listing_buy.to_excel("data/buy/" + today + "RM_buy_" + borough + ".xlsx", index = False)
     listing_rent.to_excel("data/rent/" + today + "RM_rent_" + borough + ".xlsx", index = False)
     
--- locations_dict.py
+++ locations_dict.py
@@ -1,6 +1,1 @@
-boroughs = {'clapham':'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22%7BubyHjhWjE%7CS%60QeA%3F%7DLkFwXJsOtLvBtIlLU%7CS%60NbA%3FwXxJhL%7CTpP%60K%60VvBxOmCvb%40uEtW_At_%40aIlLj%40bWtJf%60%40wVhJmh%40tQuSbHaM_z%40_Dst%40aGmq%40jQ%7DS_G%7Ba%40gQkOoLoUl%5CsAhHrH%22%7D',\
-            'richmond':'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22ob%60yH%7Cuw%40%7EHvsAa%5Bpd%40yw%40yJkNuaBtKmb%40vi%40_%5Dxa%40rt%40%22%7D',\
-           'wandsworth': 'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22kxeyHnaa%40uE%7Bp%40~NeIjOjD_DvQjEdPtWbPl_%40zKtH%60RtJxN%60Pbm%40kObOsZ%7D%60AeToMma%40_M_V%7DL%22%7D',\
-            'ravenscourt': 'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22mfhyHnbp%40cQDgIx%40eIrBaVj%40oUnBu%40ct%40bKmHzJgCz%40uP%7EVmXoDmuAkD_h%40tKiNvMgQfGpIpHl%7D%40dAdhAoBhl%40n%40%7Eu%40j%40%7CL%22%7D',\
-            'greenwich': 'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22wtdyH%7CpAaNx%60%40u%5DcAiJN%7CBgi%40%7BKuq%40%7EAoXvLg%5CxJhY%7EEr_%40pN%60CdHwItDf%7E%40%22%7D',
-            'streatham':'locationIdentifier=USERDEFINEDAREA%5E%7B%22id%22%3A6353934%7D'}
+boroughs = {'nottinghill':'locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22whkyHnrf%40oKugAnr%40sZlJha%40%7CH~l%40ky%40%7C%5E_A%7DJ%22%7D'}
